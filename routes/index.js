86cefbafadfd2270864047a804a198cefa632f3d
lilian.couto <lilian@heydancers>
2017-01-11 13:29:44 +0000

    merge the branch

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
+
+router.get('/about', function(req, res){
+  res.render('about', {
+    title: 'About',
+    content: 'This is all about me'
+  })
+})
+
+router.get('/contact', function(req, res){
+  res.render('contact',{
+    title:'Contact Us',
+    content: 'If you would like to contact us just have a browse'
+  })
+})
 
 module.exports = router;
